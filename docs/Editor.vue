11ef554806d6f587ee630daa1dd9b6941a5f8d32
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-08-23 14:51:07 +0200

    Add autocompletition

--- docs/Editor.vue
+++ docs/Editor.vue
@@ -5,8 +5,41 @@
 <script>
 import ace from 'brace';
 import debounce from 'lodash/debounce';
+import kebabCase from 'lodash/kebabCase';
+import * as components from 'src';
+import 'brace/ext/language_tools'
 import 'brace/mode/html';
 import 'brace/theme/tomorrow';
+
+const languageTools = ace.acequire('ace/ext/language_tools')
+
+const semanticUIVueCompleter = {
+  getCompletions(editor, session, pos, prefix, callback) {
+    const completions = [];
+
+    Object.values(components).forEach(({ name, props }) => {
+      completions.push({
+        caption: name,
+        value: kebabCase(name),
+        meta: 'Component',
+      });
+
+      if (props) {
+        Object.keys(props).forEach((propName) => {
+          if (completions.find(({ value }) => value === propName)) return
+          completions.push({
+            caption: propName,
+            value: kebabCase(propName),
+            meta: 'Component Prop',
+          });
+        });
+      }
+    });
+    callback(null, completions);
+  },
+};
+
+languageTools.addCompleter(semanticUIVueCompleter);
 
 export default {
   name: 'Editor',
@@ -25,17 +58,16 @@ export default {
     editor.getSession().on('change', () => {
       emitValue(editor.getValue());
     });
-    editor.session.setTabSize(2);
-    editor.renderer.setShowGutter(false);
-    editor.selection.selectFileStart();
-    editor.selection.moveTo(Infinity, Infinity);
     editor.setTheme('ace/theme/tomorrow');
     editor.setShowPrintMargin(false);
     editor.setValue(this.value);
-    editor.setOptions({
-      maxLines: Infinity
-    });
+    editor.setOptions({ maxLines: Infinity });
+    editor.setOptions({ enableBasicAutocompletion: true });
+    editor.setOptions({ enableLiveAutocompletion: true });
     editor.setReadOnly(this.readonly);
+    editor.renderer.setShowGutter(false);
+    editor.session.setTabSize(2);
+    editor.selection.moveTo(Infinity, Infinity);
   },
   template: '<div></div>',
 };
