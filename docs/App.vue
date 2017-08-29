2451bc3a02ee24249a2b199bee8a0ec496ee4e0a
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-08-29 13:44:33 +0200

    Move menu to component

--- docs/App.vue
+++ docs/App.vue
@@ -1,74 +1,6 @@
 <template>
   <div id="app" is="sui-sidebar-pushable">
-    <sui-menu
-      class="docs-sidebar"
-      is="sui-sidebar"
-      inverted="true"
-      vertical="true"
-      visible
-    >
-      <sui-menu-item>
-        <sui-image :src="`${publicPath}static/images/logo.png`" spaced="right" size="mini" />
-        <strong>
-          Semantic UI Vue
-          <small>
-            <em>{{ version }}</em>
-          </small>
-        </strong>
-      </sui-menu-item>
-      <sui-menu-item>
-        <sui-menu-header>Getting Started</sui-menu-header>
-        <sui-menu-menu>
-          <router-link is="sui-menu-item" to="/">Quick Start</router-link>
-          <router-link is="sui-menu-item" to="/contributing">
-            Contributing
-          </router-link>
-          <a is="sui-menu-item" href="https://github.com/Semantic-UI-Vue/Semantic-UI-Vue">
-            Github
-            <sui-icon name="github" />
-          </a>
-          <a is="sui-menu-item" href="https://semantic-ui.com/">
-            Semantic UI
-            <i class="icon semantic-ui"><img src="https://semantic-ui.com/favicon.ico" /></i>
-          </a>
-        </sui-menu-menu>
-      </sui-menu-item>
-      <sui-menu-item>
-        <sui-input
-          icon="search"
-          inverted
-          placeholder="Start typing..."
-          transparent
-          v-model="search"
-          @keypress.native="openFirst"
-        />
-      </sui-menu-item>
-      <sui-menu-menu v-if="search">
-        <router-link
-          is="sui-menu-item"
-          :key="entry.component"
-          :active="!index"
-          :to="entry.href"
-          v-for="(entry, index) in matchingComponents"
-        >
-          {{ entry.content }}
-          <span class="press-enter" v-if="!index">Press Enter</span>
-        </router-link>
-      </sui-menu-menu>
-      <sui-menu-item :key="mod.name" v-if="!search" v-for="mod in modules">
-        <sui-menu-header>{{ mod.name }}</sui-menu-header>
-        <sui-menu-menu>
-          <router-link
-            is="sui-menu-item"
-            :key="comp"
-            :to="getUrl(mod.name, comp)"
-            v-for="comp in mod.components"
-          >
-            {{ comp }}
-          </router-link>
-        </sui-menu-menu>
-      </sui-menu-item>
-    </sui-menu>
+    <docs-menu class="docs-sidebar" />
     <div class="docs-container">
       <router-view></router-view>
     </div>
@@ -81,65 +13,11 @@ import * as components from 'src';
 import * as collections from 'src/collections';
 import * as elements from 'src/elements';
 import * as modules from 'src/modules';
+import DocsMenu from './Menu';
 
 export default {
   name: 'app',
-  computed: {
-    matchingComponents() {
-      return this.modules
-        .map(({ name, components }) => (
-          components
-            .filter(compName => new RegExp(this.search, 'i').test(compName))
-            .map(component => ({
-              content: component,
-              href: this.getUrl(name, component),
-            }))
-        ))
-        .reduce((acc, arr) => acc.concat(arr), [])
-        .sort((a, b) => a.component > b.component)
-    }
-  },
-  data() {
-    const shouldShow = ([,component]) => (
-      !(component.meta && component.meta.parent)
-    );
-
-    return {
-      modules: [
-        {
-          name: 'Elements',
-          components: Object.entries(elements).filter(shouldShow).map(([k]) => k),
-        },
-        {
-          name: 'Collections',
-          components: Object.entries(collections).filter(shouldShow).map(([k]) => k),
-        },
-        {
-          name: 'Modules',
-          components: Object.entries(modules).filter(shouldShow).map(([k]) => k),
-        },
-      ],
-      search: '',
-      publicPath,
-      version: process.version,
-    };
-  },
-  components: {
-    Test: {
-      name: 'Test',
-      template: '<div style="border: 5px solid red"><slot /></div>',
-    },
-  },
-  methods: {
-    getUrl(mod, comp) {
-      return `/${mod}/${comp}`.toLowerCase();
-    },
-    openFirst(e) {
-      if (e.keyCode === 13 && this.search && this.matchingComponents.length) {
-        this.$router.push(this.matchingComponents[0].href);
-      }
-    },
-  },
+  components: { DocsMenu },
 };
 </script>
 
@@ -164,11 +42,6 @@ code {
   background-color: #F7F7F7;
 }
 
-.press-enter {
-  color: rgb(53, 189, 178);
-  float: right;
-}
-
 .docs-container {
   margin-left: 250px;
   min-width: 550px;
@@ -185,10 +58,6 @@ code {
   padding-bottom: 1em!important;
   background: rgb(27, 28, 29)!important;
   overflow-y: scroll!important;
-}
-
-.semantic-ui.icon img {
-  height: 12px;
 }
 
 @media screen and (max-width: 767px) {
