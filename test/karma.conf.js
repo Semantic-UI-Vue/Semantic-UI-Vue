240592330296cbfb794bc0496e3e62e2965c25f9
Mario Lamacchia <mlamacchia@hubspot.com>
2017-10-02 14:53:06 +0100

    Replace phantomjs with chrome

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -7,11 +7,7 @@ var webpackConfig = require('../build/webpack.test.conf');
 
 module.exports = function (config) {
   config.set({
-    // to run in additional browsers:
-    // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
-    // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
