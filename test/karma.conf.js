33776776cc6dadfcdfde2633086bd22100e8b9bc
Mario Lamacchia <mlamacchia@hubspot.com>
2017-10-02 15:06:55 +0100

    Test using headless chrome

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -7,7 +7,7 @@ var webpackConfig = require('../build/webpack.test.conf');
 
 module.exports = function (config) {
   config.set({
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
