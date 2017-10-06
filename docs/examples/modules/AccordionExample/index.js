b8caefdd7ff94e170d6d55fdddfe676315c8c247
Agata C <ciechanowska.agata4@gmail.com>
2017-10-06 21:03:17 +0200

    added a nested accordions example

---
+++ docs/examples/modules/AccordionExample/index.js
@@ -0,0 +1,14 @@
+import NestedAccordions from './NestedAccordions.example';
+
+export default [
+  {
+    title: 'Examples',
+    examples: [
+      {
+        title: 'Nested Accordions',
+        description: 'An accordion can have multiple levels of nested content. This content can either be in a nested accordion or simply another level of title and content',
+        component: NestedAccordions,
+      },
+    ],
+  },
+];
