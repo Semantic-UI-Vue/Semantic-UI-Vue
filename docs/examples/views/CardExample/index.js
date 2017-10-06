b7abe95f121cd8d218a847eeb44979a620f44833
Jaime Jones <jaime.madsen@gmail.com>
2017-10-06 09:04:31 -0600

    Added first two card

---
+++ docs/examples/views/CardExample/index.js
@@ -0,0 +1,20 @@
+import Card1 from './Card1.example';
+import Card2 from './Card2.example';
+
+export default [
+  {
+    title: 'Types',
+    examples: [
+      {
+        title: 'Card',
+        description: 'A single card',
+        component: Card1,
+      },
+      {
+        title: '',
+        description: '',
+        component: Card2,
+      },
+    ],
+  },
+];
--- docs/examples/views/index.js
+++ docs/examples/views/index.js
@@ -1,2 +1,3 @@
+export { default as CardExample } from './CardExample';
 export { default as CommentExample } from './CommentExample';
 export { default as StatisticExample } from './StatisticExample';
