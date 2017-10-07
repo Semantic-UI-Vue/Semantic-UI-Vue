c8da285deb8d9c9d897703be2852130a5cfd8776
Amit Mittal <mittalmailbox@gmail.com>
2017-10-07 17:35:18 +0530

    Added example for rating module (#43)

--- docs/examples/modules/index.js
+++ docs/examples/modules/index.js
@@ -1,2 +1,3 @@
 export { default as ModalExample } from './ModalExample';
+export { default as RatingExample } from './RatingExample';
 export { default as CheckboxExample } from './CheckboxExample';
