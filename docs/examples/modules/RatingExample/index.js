89529313427ab293fd88540125d8eb4ad6219215
Amit Mittal <mittalmailbox@gmail.com>
2017-10-07 13:07:04 +0530

    Added example for rating module

---
+++ docs/examples/modules/RatingExample/index.js
@@ -0,0 +1,14 @@
+import Rating from './Rating.example';
+
+export default [
+  {
+    title: 'Types',
+    examples: [
+      {
+        title: 'Rating',
+        description: 'A basic rating element',
+        component: Rating,
+      },
+    ],
+  },
+];
