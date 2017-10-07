e4d80203e74c32e862e987d349d44444ae2b0c69
Agata C <ciechanowska.agata4@gmail.com>
2017-10-07 21:37:36 +0200

    added form fields accordion example

--- docs/examples/modules/AccordionExample/index.js
+++ docs/examples/modules/AccordionExample/index.js
@@ -1,4 +1,5 @@
 import NestedAccordions from './NestedAccordions.example';
+import FormFieldsAccordion from './FormFieldsAccordion.example';
 
 export default [
   {
@@ -8,6 +9,11 @@ export default [
         title: 'Nested Accordions',
         description: 'An accordion can have multiple levels of nested content. This content can either be in a nested accordion or simply another level of title and content',
         component: NestedAccordions,
+      },
+      {
+        title: 'Form Fields',
+        description: 'An accordion can be used anywhere where content can be shown or hidden. For example, to show optional form fields.',
+        component: FormFieldsAccordion,
       },
     ],
   },
