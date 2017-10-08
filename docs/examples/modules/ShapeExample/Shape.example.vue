a316015a32f7aad00b25bb70c54aad285b33633a
Dom Corvasce <dom.corvasce@yandex.com>
2017-10-08 12:57:43 +0200

    update shape example

--- docs/examples/modules/ShapeExample/Shape.example.vue
+++ docs/examples/modules/ShapeExample/Shape.example.vue
@@ -1,24 +1,28 @@
 <template lang="html">
   <div>
-    <sui-shape text="true">
-      <sui-shape-side class="header">Did you know? This side starts visible.</sui-shape-side>
-      <sui-shape-side class="header">Help, its another side!</sui-shape-side>
-      <sui-shape-side class="header">This is the last side</sui-shape-side>
+    <sui-shape :text="true">
+      <div class="sides">
+        <sui-shape-side class="header" :active="true">Did you know? This side starts visible.</sui-shape-side>
+        <sui-shape-side class="header">Help, its another side!</sui-shape-side>
+        <sui-shape-side class="header">This is the last side</sui-shape-side>
+      </div>
     </sui-shape>
 
-    <sui-button class="flip-up-btn">
-      <sui-icon class="up long arrow"></sui-icon>
-    </sui-shape-button>
+    <div style="margin-top:15px">
+      <sui-button class="flip-up-btn" v-on:click="flipUp" content="Flip up"></sui-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ShapeExample',
-  created() {
-    $('.flip-up-btn').on('click', (_event) => {
-      $('.shape').shape('flip up');
-    });
+  methods: {
+    flipUp: (_event) => {
+      $('.flip-up-btn').on('click', (_event) => {
+        $('.shape').shape('flip up');
+      });
+    },
   },
 };
 </script>
