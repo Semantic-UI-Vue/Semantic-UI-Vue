97baae8b3708e12e4d5d0e318c7731a9e93ed939
Dom Corvasce <dom.corvasce@yandex.com>
2017-10-08 16:53:55 +0200

    add spec for shape side component

---
+++ test/specs/modules/Shape/ShapeSide.spec.js
@@ -0,0 +1,17 @@
+import { shallow } from 'vue-test-utils';
+import { testClassFromProps } from 'test/utils';
+import ShapeSide from 'src/modules/Shape/ShapeSide';
+
+describe('ShapeSide', () => {
+  testClassFromProps(ShapeSide, ['side']);
+
+  it('should create an active side', () => {
+    const side = shallow(ShapeSide, { propsData: { active: true } });
+    expect(side.element.classList.contains('active')).to.equal(true);
+  });
+
+  it('should create a hidden side', () => {
+    const side = shallow(ShapeSide, { propsData: {} });
+    expect(side.element.classList.contains('active')).to.equal(false);
+  });
+});
