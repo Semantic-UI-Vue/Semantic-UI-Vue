782ed40496cd768401722a5ca030a6147ae6fbac
Dom Corvasce <dom.corvasce@yandex.com>
2017-10-08 12:03:49 +0200

    add shape example

---
+++ docs/examples/modules/ShapeExample/Shape.example.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div>
+    <sui-shape :text>
+      <sui-shape-side class="header">Did you know? This side starts visible.</sui-shape-side>
+      <sui-shape-side class="header">Help, its another side!</sui-shape-side>
+      <sui-shape-side class="header">This is the last side</sui-shape-side>
+    </sui-shape>
+
+    <sui-button class="flip-up-btn">
+      <sui-icon class="up long arrow"></sui-icon>
+    </sui-shape-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ShapeExample',
+    created() {
+      $('.flip-up-btn').on('click', (_event) => {
+        $('.shape').shape('flip up');
+      });
+    },
+  };
+</script>
+
+<style lang="css">
+</style>
---
+++ docs/examples/modules/ShapeExample/index.js
@@ -0,0 +1,1 @@
+import Shape from 'Shape.example';
