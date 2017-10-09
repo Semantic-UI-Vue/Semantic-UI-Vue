35e96595618f6efa764bd086ed94a219270f32a0
Ildar Gilfanov <ildarrazin@gmail.com>
2017-10-09 14:55:24 +0300

    [RFR] Progress module (#50)
    * feat(PropTypes): add state
    
    * feat(elements): add Progress
    
    * feat(docs): add progress examples
    
    * refactor(Progress): move from elements to modules

---
+++ docs/examples/modules/ProgressExample/Standart.example.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div>
+    <sui-progress :percent=percent label="Uploading Files"/>
+    <sui-button @click.native="decrease">-</sui-button>
+    <sui-button @click.native="increase">+</sui-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StandartProgressExample',
+  data() {
+    return { percent: 10 };
+  },
+  methods: {
+    decrease() {
+      this.percent -= 17;
+      if (this.percent <= 0) {
+        this.percent = 0;
+      }
+    },
+    increase() {
+      this.percent += 13;
+      if (this.percent >= 100) {
+        this.percent = 100;
+      }
+    },
+  },
+};
+</script>
