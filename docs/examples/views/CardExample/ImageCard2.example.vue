00d7ec61b1be521b7d72d4d428b8e1a8a43af582
Jaime Jones <jaime.madsen@gmail.com>
2017-10-11 16:00:17 -0600

    ImageCard2 example works

--- docs/examples/views/CardExample/ImageCard2.example.vue
+++ docs/examples/views/CardExample/ImageCard2.example.vue
@@ -2,16 +2,14 @@
   <div>
     <sui-card-group :itemsPerRow="3">
       <sui-card>
-         <sui-image
-          src="static/images/avatar/large/elliot.jpg"
-          is="sui-dimmer-dimmable"
-          @mouseover.native="isActive[0] = true"
-          @mouseleave.native="isActive[0] = false"
-        >
-        <sui-dimmer blurring :active="isActive[0]">
-          <sui-button inverted>Add Friend</sui-button>
-        </sui-dimmer>
-      </sui-image>
+        <sui-dimmer-dimmable
+          @mouseenter.native="cardOneActive = true"
+          @mouseleave.native="cardOneActive = false">
+          <sui-image src="static/images/avatar/large/elliot.jpg" />
+          <sui-dimmer blurring :active="cardOneActive">
+            <sui-button inverted>Add Friend</sui-button>
+          </sui-dimmer>
+        </sui-dimmer-dimmable>
         <sui-card-content>
           <sui-card-header>Team Fu</sui-card-header>
           <sui-card-meta>Create in Sep 2014</sui-card-meta>
@@ -21,15 +19,14 @@
           </sui-card-content>
       </sui-card>
       <sui-card>
-         <sui-image
-          src="static/images/avatar/large/jenny.jpg"
-          is="sui-dimmer-dimmable"
-          @mouseover.native="isActive[1] = true"
-          @mouseleave.native="isActive[1] = false"
-        >
-        <sui-dimmer blurring inverted :active="isActive[1]">
-          <sui-button primary>Add Friend</sui-button>
-        </sui-dimmer>
+        <sui-dimmer-dimmable
+          @mouseenter.native="cardTwoActive = true"
+          @mouseleave.native="cardTwoActive = false">
+          <sui-image src="static/images/avatar/large/jenny.jpg" />
+          <sui-dimmer blurring inverted :active="cardTwoActive">
+            <sui-button primary>Add Friend</sui-button>
+          </sui-dimmer>
+        </sui-dimmer-dimmable>
       </sui-image>
         <sui-card-content>
           <sui-card-header>Team Hess</sui-card-header>
@@ -48,7 +45,10 @@
 export default {
   name: 'ImageCard2Example',
   data() {
-    return { isActive: [] };
+    return {
+      cardOneActive: false,
+      cardTwoActive: false
+     };
   },
 };
 </script>
