e06a7c670b72baf3dce2949afedcd5223c9b68ea
Dom Corvasce <dom.corvasce@yandex.com>
2017-10-13 09:54:11 +0200

    add props to animate shape

--- src/modules/Shape/Shape.jsx
+++ src/modules/Shape/Shape.jsx
@@ -1,5 +1,6 @@
 import { classes, getElementType, getChildProps } from '../../lib';
 import { Enum } from '../../lib/PropTypes';
+import ShapeSide from './ShapeSide';
 
 export default {
   name: 'SuiShape',
@@ -8,6 +9,36 @@ export default {
       type: Boolean,
       default: false,
       description: 'Transform the shape in a cube.',
+    },
+    activeIndex: {
+      type: Number,
+      default: 0,
+      description: 'The index of the active side.',
+    },
+    animation: {
+      type: Enum(['flip left', 'flip up', 'flip down', 'flip right', 'flip over', 'flip back']),
+      description: 'The animation for the shape.',
+    },
+    duration: {
+      type: Number,
+      default: 25000,
+      description: 'The animation duration.',
+    },
+    width: {
+      type: String,
+      default: 'initial',
+    },
+    height: {
+      type: String,
+      default: 'initial',
+    },
+    items: {
+      type: Array,
+      description: 'Shorthand for shape sides.',
+    },
+    styles: {
+      type: Object,
+      description: 'The style rules for the shape sides.',
     },
     text: {
       type: Boolean,
--- src/modules/Shape/ShapeSide.jsx
+++ src/modules/Shape/ShapeSide.jsx
@@ -8,6 +8,16 @@ export default {
       default: false,
       description: 'Set the first side to be showed.',
     },
+    animating: {
+      type:  Boolean,
+      default: false,
+      description: 'The side is currently animating.',
+    },
+    hidden: {
+      type: Boolean,
+      default: false,
+      description: 'THe side is currently hidden.',
+    },
     header: {
       type: Boolean,
       default: false,
@@ -16,15 +26,35 @@ export default {
   },
   render() {
     const ElementType = getElementType(this);
+    let style = {};
+
+    if (classes(this.animating)) {
+      style = {
+        left: '0',
+        transform: 'rotateY(-90deg) translateZ(145px)',
+        transitionDuration: `${duration}ms`,
+      };
+    }
+
+    if (classes(this.active) && classes(this.hidden)) {
+      style = {
+        transform: 'rotateY(0deg) translateZ(145px)',
+        transitionDuration: `${duration}ms`,
+      };
+    }
 
     return(
       <ElementType
         {...getChildProps(this)}
         class={classes(
           this.active && 'active',
+          this.animating && 'animating',
           this.header && 'header',
+          this.hidden && 'hidden',
           'side',
         )}
+
+        style={style}
       >
         {this.$slots.default}
       </ElementType>
