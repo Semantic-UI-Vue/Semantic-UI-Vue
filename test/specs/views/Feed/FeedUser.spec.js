9d43e1d1a4291b3d58ecc5e50fce6b98be001927
Truong Viet Trung <vitruong@expedia.com>
2017-10-13 14:00:10 +0800

    Add test for FeedLike

--- test/specs/views/Feed/FeedUser.spec.js
+++ test/specs/views/Feed/FeedUser.spec.js
@@ -4,21 +4,22 @@ import FeedUser from 'src/views/Feed/FeedUser';
 describe('FeedUser', () => {
   it('should create a SUI FeedUser', () => {
     const user = shallow(FeedUser);
-    expect(user.is('a'));
-    expect(user.hasClass('user'));
+    expect(user.is('a')).to.equal(true);
+    expect(user.hasClass('user')).to.equal(true);
+    expect(user.text()).to.equal('');
   });
 
   it('should create a SUI FeedUser with content', () => {
-    const user = shallow(FeedUser, { propsData: { content: 'Eliwood' } });
-    expect(user.is('a'));
-    expect(user.hasClass('user'));
-    expect(user.text()).to.equal('Eliwood');
+    const user = shallow(FeedUser, { propsData: { content: 'Elliot' } });
+    expect(user.is('a')).to.equal(true);
+    expect(user.hasClass('user')).to.equal(true);
+    expect(user.text()).to.equal('Elliot');
   });
 
   it('should create a SUI FeedUser with default slot', () => {
-    const user = shallow(FeedUser, { slots: { default: '<span>Hector</span>' } });
-    expect(user.is('a'));
-    expect(user.hasClass('user'));
-    expect(user.text()).to.equal('Hector');
+    const user = shallow(FeedUser, { slots: { default: '<span>Hera</span>' } });
+    expect(user.is('a')).to.equal(true);
+    expect(user.hasClass('user')).to.equal(true);
+    expect(user.text()).to.equal('Hera');
   });
 });
