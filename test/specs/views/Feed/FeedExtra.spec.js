ebac8a1f084b97a495d53fbbe000c3ea1988b920
Truong Viet Trung <vitruong@expedia.com>
2017-10-13 16:18:36 +0800

    Add tests for FeedExtra

---
+++ test/specs/views/Feed/FeedExtra.spec.js
@@ -0,0 +1,48 @@
+import { shallow } from 'vue-test-utils';
+import FeedExtra from 'src/views/Feed/FeedExtra';
+
+describe('FeedExtra', () => {
+  it('should create a SUI FeedExtra', () => {
+    const extra = shallow(FeedExtra);
+    expect(extra.is('div')).to.equal(true);
+    expect(extra.hasClass('extra')).to.equal(true);
+    expect(extra.text()).to.equal('');
+  });
+
+  it('should create a SUI FeedExtra with content', () => {
+    const extra = shallow(FeedExtra, { propsData: { content: 'More text here' } });
+    expect(extra.is('div')).to.equal(true);
+    expect(extra.hasClass('extra')).to.equal(true);
+    expect(extra.hasClass('text')).to.equal(true);
+    expect(extra.text()).to.equal('More text here');
+  });
+
+  it('should create a SUI FeedExtra with images class', () => {
+    const extra = shallow(FeedExtra, { propsData: { images: true } });
+    expect(extra.is('div')).to.equal(true);
+    expect(extra.hasClass('extra')).to.equal(true);
+    expect(extra.hasClass('images')).to.equal(true);
+    expect(extra.contains('img')).to.equal(false);
+  });
+
+  it('should create a SUI FeedExtra with images', () => {
+    const extra = shallow(FeedExtra, { propsData: { images: [
+      '/image0.png',
+      '/image1.png',
+    ] } });
+    expect(extra.is('div')).to.equal(true);
+    expect(extra.hasClass('extra')).to.equal(true);
+    expect(extra.hasClass('images')).to.equal(true);
+    expect(extra.contains('img')).to.equal(true);
+    expect(extra.findAll('img').length).to.equal(2);
+    expect(extra.findAll('img').at(0).hasAttribute('src', '/image0.png')).to.equal(true);
+    expect(extra.findAll('img').at(1).hasAttribute('src', '/image1.png')).to.equal(true);
+  });
+
+  it('should create a SUI FeedExtra with default slot', () => {
+    const extra = shallow(FeedExtra, { slots: { default: '<span>More text here</span>' } });
+    expect(extra.is('div')).to.equal(true);
+    expect(extra.hasClass('extra')).to.equal(true);
+    expect(extra.text()).to.equal('More text here');
+  });
+});
