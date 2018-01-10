b48ebf05103b37e9ab4261c32a7f108e5b7ea9a3
Mario Lamacchia <mlamacchia@hubspot.com>
2018-01-10 15:12:37 +0100

    Add radio

--- docs/examples/modules/CheckboxExample/index.js
+++ docs/examples/modules/CheckboxExample/index.js
@@ -1,6 +1,8 @@
 import CheckboxBasic from './CheckboxBasic.example';
 import CheckboxModel from './CheckboxModel.example';
 import CheckboxToggle from './CheckboxToggle.example';
+import Radio from './Radio.example';
+import RadioGroupInline from './RadioGroupInline.example';
 
 export default [
   {
@@ -10,6 +12,14 @@ export default [
         title: 'Checkbox Basic',
         description: 'A standard Checkbox',
         component: CheckboxBasic,
+      },
+      {
+        title: 'Radio',
+        description: 'A checkbox can be formatted as a radio element. This means it is an exclusive option',
+        component: Radio,
+      },
+      {
+        component: RadioGroupInline,
       },
       {
         title: 'Checkbox with two way binding',
