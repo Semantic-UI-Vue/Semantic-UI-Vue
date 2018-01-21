00a8139b272ff05df935b2724a39be8b8766916f
Arsen Stahanov <arsen.stahanov@gmail.com>
2018-01-21 20:43:58 +0200

    Add `fullWidth` prop to Table-Header and Table-Footer

--- src/collections/Table/TableFooter.jsx
+++ src/collections/Table/TableFooter.jsx
@@ -1,11 +1,17 @@
-import { getChildProps, getElementType } from '../../lib';
+import { classes, getChildProps, getElementType } from '../../lib';
 
 export default {
   name: 'SuiTableFooter',
+  props: {
+    fullWidth: Boolean,
+  },
   render() {
     const ElementType = getElementType(this, 'tfoot');
     return (
-      <ElementType {...getChildProps(this)}>
+      <ElementType
+        {...getChildProps(this)}
+        class={classes(this.fullWidth && 'full-width')}
+      >
         {this.$slots.default}
       </ElementType>
     );
