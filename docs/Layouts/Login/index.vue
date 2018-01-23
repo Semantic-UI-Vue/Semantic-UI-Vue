b024479c2da945f276b336fc9d2951044a4f5b3f
Curtis Whitby <cwhitby@users.noreply.github.com>
2018-01-23 12:56:05 +0000

    Add login layout (#61) (#84)

---
+++ docs/Layouts/Login/index.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+  <sui-grid centered vertical-align="middle">
+    <sui-grid-column>
+
+      <h2 is="sui-header" color="teal" image>
+        <sui-image src="static/images/logo.png" />
+        <sui-header-content>Log-in to your account</sui-header-content>
+      </h2>
+
+      <sui-form>
+        <sui-segment stacked>
+          <sui-form-field>
+            <sui-input
+              type="email"
+              placeholder="E-mail address"
+              icon="user"
+              icon-position="left" />
+          </sui-form-field>
+          <sui-form-field>
+            <sui-input
+              type="password"
+              placeholder="Password"
+              icon="lock"
+              icon-position="left" />
+          </sui-form-field>
+          <sui-button size="large" color="teal" fluid>Login</sui-button>
+        </sui-segment>
+      </sui-form>
+
+      <sui-message>New to us? <a href="#">Sign Up</a></sui-message>
+
+    </sui-grid-column>
+  </sui-grid>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="css">
+  body {
+    background-color: #DADADA !important;
+  }
+
+  #app, .grid {
+    height: 100%;
+  }
+
+  .column {
+    max-width: 450px;
+    text-align: center !important;
+  }
+</style>
