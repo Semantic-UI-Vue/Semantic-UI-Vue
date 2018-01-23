de2ac5fd550fa4fb20482c70161aebde39f6c0af
Arsen Stahanov <arsen.stahanov@gmail.com>
2018-01-23 16:37:36 +0200

    Add some examples for table

--- docs/examples/collections/TableExample/index.js
+++ docs/examples/collections/TableExample/index.js
@@ -3,6 +3,9 @@ import TableAvatar from './TableAvatar.example';
 import TableDefinition from './TableDefinition.example';
 import TableGit from './TableGit.example';
 import TableRating from './TableRating.example';
+import TableColored from './TableColored.example';
+import TableColoredInverted from './TableColoredInverted.example';
+import TableFullWidth from './TableFullWidth.example';
 
 export default [
   {
@@ -26,6 +29,21 @@ export default [
         title: 'Definition',
         description: 'A table may be formatted to emphasize a first column that defines a rows content',
         component: TableDefinition,
+      },
+      {
+        title: 'Colored',
+        description: 'A table can be given a color to distinguish it from other tables.',
+        component: TableColored,
+      },
+      {
+        title: 'Inverted',
+        description: 'A table\'s colors can be inverted.',
+        component: TableColoredInverted,
+      },
+      {
+        title: 'Full-Width Header / Footer',
+        description: 'A definition table can have a full width header or footer, filling in the gap left by the first column',
+        component: TableFullWidth,
       },
     ],
   },
