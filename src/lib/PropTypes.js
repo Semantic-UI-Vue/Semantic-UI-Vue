0216f240ad59ec4c9d418547509944ba2e3231df
Arsen <arsen.stahanov@gmail.com>
2018-01-29 18:08:38 +0200

    Add Social Enum to lib

--- src/lib/PropTypes.js
+++ src/lib/PropTypes.js
@@ -29,3 +29,4 @@ Enum.Color = Enum([
   'violet', 'purple', 'pink', 'brown', 'grey', 'black',
 ]);
 Enum.VerticalAlign = Enum(['top', 'middle', 'bottom']);
+Enum.Social = Enum(['facebook', 'twitter', 'google', 'vk', 'instagram', 'linkedin', 'youtube']);
