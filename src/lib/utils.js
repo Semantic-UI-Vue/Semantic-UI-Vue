8880410ce07834d47db907fe4fdbe5a27dceefe6
Arsen <arsen.stahanov@gmail.com>
2018-01-30 20:07:45 +0200

    Improve classes function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,19 @@
 import uniq from 'lodash/uniq';
 
 export const classes = (...classList) => (
-  uniq(classList.filter(c => !!c).join(' ').split(' ')).join(' ')
+  uniq(classList.map((c) => {
+    if (Array.isArray(c)) {
+      const cond = c[0];
+      if (cond || cond === '') {
+        if (typeof cond !== 'string' || cond === '') {
+          c.shift();
+        }
+        return c.join(' ');
+      }
+      return false;
+    }
+    return c;
+  }).filter(c => !!c).join(' ').split(' ')).join(' ')
 );
 
 export const num = i => ([
