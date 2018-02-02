962af736a16c4cf6fbe0f2b62dadbab30c8a3bc3
Agney <agney@dexlock.com>
2018-02-02 15:30:57 +0530

    #77 remove hasClass from testClassFromProps

--- test/utils.js
+++ test/utils.js
@@ -3,6 +3,6 @@ import { shallow } from 'vue-test-utils';
 export function testClassFromProps(Component, props) {
   props.forEach(prop => it(`should have class ${prop}`, () => {
     const wrapper = shallow(Component, { propsData: { [prop]: true } });
-    expect(wrapper.hasClass(prop)).to.equal(true);
+    expect(wrapper.classes()).to.include(prop);
   }));
 }
