732be433ab6298d8bbf9386a13e22e03d4ee049b
Agney <agney@dexlock.com>
2018-02-02 17:52:08 +0530

    segment tests

--- test/specs/elements/Segment/Segment.spec.js
+++ test/specs/elements/Segment/Segment.spec.js
@@ -1,8 +1,21 @@
+import { shallow } from 'vue-test-utils';
 import { testClassFromProps } from 'test/utils';
-import Segment from 'src/elements/Segment/Button';
+import Segment from 'src/elements/Segment/Segment';
 
 describe('Segment', () => {
   testClassFromProps(Segment, [
     'attached', 'basic', 'padded', 'inverted', 'stacked', 'piled', 'raised', 'vertical',
   ]);
+
+  it('should create a SUI Segment', () => {
+    const segment = shallow(Segment);
+    expect(segment.is('div')).to.equal(true);
+    expect(segment.hasClass('ui')).to.equal(true);
+    expect(segment.hasClass('segment')).to.equal(true);
+  });
+
+  it('should have content', () => {
+    const segment = shallow(Segment, { slots: { default: '<span>bar</span>' } });
+    expect(segment.text()).to.equal('bar');
+  });
 });
