d1516ecc5a362633aab3067a6ccb8db09e7c4b09
Mario Lamacchia <mlamacchia@hubspot.com>
2018-02-04 21:05:57 +0000

    Add avatar

--- src/modules/Dropdown/DropdownItem.jsx
+++ src/modules/Dropdown/DropdownItem.jsx
@@ -1,8 +1,13 @@
 import { classes, getChildProps, getElementType } from '../../lib';
+import Image from '../../elements/Image/Image';
 
 export default {
   name: 'SuiDropdownItem',
   props: {
+    image: {
+      type: Object,
+      description: 'Shorthand for Image.',
+    },
     text: {
       type: String,
       description: 'Display text.',
@@ -16,6 +21,7 @@ export default {
     const ElementType = getElementType(this);
     return (
       <ElementType {...getChildProps(this)} role="option" class={classes('item')}>
+        {this.image && <Image {...{ props: this.image }} />}
         {this.text || this.$slots.default}
       </ElementType>
     );
