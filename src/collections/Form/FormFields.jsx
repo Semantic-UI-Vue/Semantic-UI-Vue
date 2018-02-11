5dcd23ab8e052aa4ca262f89d554f36edf81a6af
Arsen <arsen.stahanov@gmail.com>
2018-02-11 09:19:09 +0200

    Add `fields` prop to FormFields

--- src/collections/Form/FormFields.jsx
+++ src/collections/Form/FormFields.jsx
@@ -1,17 +1,23 @@
-import { classes, getChildProps, getElementType } from '../../lib';
+import { num, classes, getChildProps, getElementType } from '../../lib';
 
 export default {
   name: 'SuiFormFields',
   props: {
     inline: Boolean,
     grouped: Boolean,
+    fields: Number,
   },
   render() {
     const ElementType = getElementType(this);
     return (
       <ElementType
         {...getChildProps(this)}
-        class={classes(this.inline && 'inline', this.grouped && 'grouped', 'fields')}
+        class={classes(
+          num(this.fields),
+          this.inline && 'inline',
+          this.grouped && 'grouped',
+          'fields',
+        )}
       >
         {this.$slots.default}
       </ElementType>
