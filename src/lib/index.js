94ad842ddff6b73fe36606583f5022093b48cb4f
Arsen <arsen.stahanov@gmail.com>
2018-02-12 06:41:16 +0200

    Add animation to dropdown menu

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,4 @@ export { default as getChildProps } from './getChildProps';
 export { default as getElementType } from './getElementType';
 export * from './mixins';
 export * from './utils';
+export * from './animationHelper';
--- src/modules/Dropdown/DropdownMenu.jsx
+++ src/modules/Dropdown/DropdownMenu.jsx
@@ -1,9 +1,44 @@
-import { classes, getChildProps, getElementType } from '../../lib';
+import { getEventAnimationEnd, classes, getChildProps, getElementType } from '../../lib';
+
+const states = {
+  closed: 'closed',
+  opening: 'opening',
+  open: 'open',
+  closing: 'closing',
+};
+
+const animation = 'slide down';
+
+function buildAnimation(name, direction) {
+  return `transition animating ${name} ${direction ? 'in' : 'out'} visible`;
+}
 
 export default {
   name: 'SuiDropdownMenu',
   data() {
-    return { open: false };
+    return {
+      open: false,
+      visualState: states.closed,
+    };
+  },
+  computed: {
+    classes() {
+      switch (this.visualState) {
+        case states.opening:
+          return `${buildAnimation(animation, true)} active`;
+        case states.open:
+          return 'visible active';
+        case states.closing:
+          return buildAnimation(animation, false);
+        default:
+          return '';
+      }
+    },
+  },
+  watch: {
+    open(newValue) {
+      this.visualState = newValue ? states.opening : states.closing;
+    },
   },
   mounted() {
     let parent = this.$parent;
@@ -18,10 +53,13 @@ export default {
     if (!this.dropdown) {
       throw new Error('SuiDropdownMenu must be place as a child of a SuiDropdown');
     }
-
     this.dropdown.register(this);
+    this.$el.addEventListener(getEventAnimationEnd(), this.onAnimationEnded, false);
   },
   methods: {
+    onAnimationEnded() {
+      this.visualState = this.open ? states.open : states.closed;
+    },
     setOpen(open) {
       this.open = open;
     },
@@ -31,7 +69,7 @@ export default {
     return (
       <ElementType
         {...getChildProps(this)}
-        class={classes('menu', this.open && 'visible', 'transition')}
+        class={classes('menu', this.open && 'visible', 'transition', this.classes)}
       >
         {this.$slots.default}
       </ElementType>
