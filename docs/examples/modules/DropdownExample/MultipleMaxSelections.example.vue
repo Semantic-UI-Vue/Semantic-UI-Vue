1ca571f4cab15d6b7a009901aa4ed9fa5ac5c09b
Arsen <arsen.stahanov@gmail.com>
2018-02-13 17:30:52 +0200

    Fix wrong names of examples

--- docs/examples/modules/DropdownExample/MultipleMaxSelections.example.vue
+++ docs/examples/modules/DropdownExample/MultipleMaxSelections.example.vue
@@ -12,7 +12,7 @@
 
 <script>
 export default {
-  name: 'SearchSelectionExample',
+  name: 'MultipleMaxSelections',
   data() {
     return {
       current: null,
--- docs/examples/modules/DropdownExample/SelectionWithIcons.example.vue
+++ docs/examples/modules/DropdownExample/SelectionWithIcons.example.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  name: 'SelectionExample',
+  name: 'SelectionWithIcons',
   data() {
     return {
       current: null,
