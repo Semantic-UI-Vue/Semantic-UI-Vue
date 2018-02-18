0a1426f99f33774932ad4a64520ac86b3dcff68e
Arsen Stahanov <30985699+ArStah@users.noreply.github.com>
2018-02-18 13:30:33 +0200

    Grid missing props (#116)
    * [Grid]: Add `textAlign` prop
    
    * [Grid]: Add `container` prop
    
    * [Grid]: Add `reversed` prop
    
    * [Grid]: Add `verticalReversed` prop
    
    * [GridColumn]: Add `centered` prop
    
    * [GridColumn]: Add `textAlign` prop
    
    * [GridColumn]: Add `floated` prop
    
    * [GridColumn]: Add `only` prop
    
    * [GridColumn]: Add `stretched` prop
    
    * [GridColumn]: Add props for different sizes on different screens
    
    * [GridRow]: Add `centered` prop
    
    * [GriRow]: Add `only` prop
    
    * [GridRow]: Add `divided` prop
    
    * [GridRow]: Add `color` prop
    
    * [GridRow]: Fix `column` class position
    
    * [Utils]: Delete extra semicolon
    
    * [GridRow]: Add `reversed` prop
    
    * [GridRow]: Add `Stretched` prop
    
    * [GridRow]: Add `textAlign` & `verticalAlign` prop
    
    * [GridColumn]: Add `verticalAlign` prop
    
    * [GridColumn]: Add description to `color` prop
    
    * [GridColumn]: Add description to `floated` prop
    
    * [GridColumn]: Add description to `textAlign` prop
    
    * [GridColumn]: Add description to `verticalAlign` prop
    
    * [GridRow]: Add description to `verticalAlign` prop
    
    * [GridRow]: Add description to `textAlign` prop
    
    * [GridRow]: Add description to `columns` prop
    
    * [Grid]: Improve `celled` prop
    
    * [Grid]: Add description to `centered` prop
    
    * [Grid]: Add description to `doubling` prop
    
    * [Grid]: Add description to `divided` prop
    
    * [Grid]: Add description to `relaxed` prop
    
    * [Grid]: Add description to `stackable` prop
    
    * [Grid]: Improve `padded` prop
    
    * [Grid]: Add description to `textAlign` & `verticallyAlign` props
    
    * [Grid]: Add description to `container` prop
    
    * [PropTypes]: Create `textAlign` enum
    
    * [Grid*]: Embed `textAlign` enum
    
    * [Grid*]: Embed `verticalAlign` enum

--- src/collections/Grid/GridRow.jsx
+++ src/collections/Grid/GridRow.jsx
@@ -1,10 +1,41 @@
-import { classes, getChildProps, getElementType, num } from '../../lib';
+import { classes, getChildProps, getElementType, num, textAlign } from '../../lib';
+import { Enum } from '../../lib/PropTypes';
 
 export default {
   name: 'SuiGridRow',
   description: 'A row sub-component for Grid',
   props: {
-    columns: Number,
+    columns: {
+      type: Number,
+      description: 'Represents column count per line in Row.',
+    },
+    centered: {
+      type: Boolean,
+      description: 'A row can have its columns centered.',
+    },
+    only: Enum(['mobile', 'tablet', 'computer', 'widescreen', 'large screen'], {
+      description: 'A row can appear only for a specific device, or screen sizes.',
+    }),
+    divided: {
+      type: Boolean,
+      description: 'A row can have dividers between its columns.',
+    },
+    color: Enum.Color({
+      description: 'A grid row can be colored.',
+    }),
+    reversed: Enum(['mobile', 'tablet', 'computer'], {
+      description: 'A row can specify that its columns should reverse order at different device types.',
+    }),
+    stretched: {
+      type: Boolean,
+      description: 'A row can stretch its contents to take up the entire column height.',
+    },
+    textAlign: Enum.TextAlign({
+      description: 'A row can specify its text alignment.',
+    }),
+    verticalAlign: Enum.VerticalAlign({
+      description: 'A row can specify its vertical alignment to have all its columns vertically centered.',
+    }),
   },
   render() {
     const ElementType = getElementType(this);
@@ -12,6 +43,14 @@ export default {
       <ElementType
         {...getChildProps(this)}
         class={classes(
+          this.reversed && `${this.reversed} reversed`,
+          this.only && `${this.only} only`,
+          this.divided && 'divided',
+          textAlign(this.textAlign),
+          this.verticalAlign && `${this.verticalAlign} aligned`,
+          this.centered && 'centered',
+          this.stretched && 'stretched',
+          this.color,
           num(this.columns),
           this.columns && 'column',
           'row',
