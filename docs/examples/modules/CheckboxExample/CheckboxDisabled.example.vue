0403cb01546c7e3a6edce2c019bb7503aac05958
Jason Truesdell <beducky@hotmail.com>
2018-02-19 16:50:59 +0900

    Allow disabling checkboxes, explicitly create radio buttons
    - Use input type=radio when checkbox is of type Radio (to avoid some
    subtle defects I was seeing when the underlying type was checkbox)
    - Add support for SUI's disabled attribute on checkbox
    - Add examples for radio button
    - Add examples for disabled attribute
    - Add tests

---
+++ docs/examples/modules/CheckboxExample/CheckboxDisabled.example.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div>
+    <sui-checkbox label="checkbox" disabled />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxBasicExample',
+};
+</script>
+
+<style lang="css">
+</style>
