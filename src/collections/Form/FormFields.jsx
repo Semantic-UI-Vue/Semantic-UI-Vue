acfb5f4c424058745184321d8ff4e7d054762f83
Arsen <arsen.stahanov@gmail.com>
2018-02-22 19:10:05 +0200

    [FormFields] Add description to props

--- src/collections/Form/FormFields.jsx
+++ src/collections/Form/FormFields.jsx
@@ -3,9 +3,22 @@ import { num, classes, getChildProps, getElementType } from '../../lib';
 export default {
   name: 'SuiFormFields',
   props: {
-    inline: Boolean,
-    grouped: Boolean,
-    fields: Number,
+    inline: {
+      type: Boolean,
+      description: 'Multiple fields may be inline in a row.',
+    },
+    grouped: {
+      type: Boolean,
+      description: 'Fields can show related choices.',
+    },
+    fields: {
+      type: Number,
+      description: 'Represents number of fields in group',
+    },
+    unstackable: {
+      type: Boolean,
+      description: 'A form group can prevent itself from stacking on mobile.',
+    },
   },
   render() {
     const ElementType = getElementType(this);
@@ -13,6 +26,7 @@ export default {
       <ElementType
         {...getChildProps(this)}
         class={classes(
+          this.unstackable && 'unstackable',
           num(this.fields),
           this.inline && 'inline',
           this.grouped && 'grouped',
