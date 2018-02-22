db12a836d0b109a383f6edb3880bf79a770d8aca
Arsen <arsen.stahanov@gmail.com>
2018-02-22 18:13:48 +0200

    [Form] Add `size` prop

--- src/collections/Form/Form.jsx
+++ src/collections/Form/Form.jsx
@@ -1,13 +1,23 @@
 import { classes, getChildProps, getElementType } from '../../lib';
+import { Enum } from '../../lib/PropTypes';
 
 export default {
   name: 'SuiForm',
+  props: {
+    size: Enum.Size({
+      description: 'A form can vary in size.',
+    }),
+  },
   render() {
     const ElementType = getElementType(this);
     return (
       <ElementType
         {...getChildProps(this)}
-        class={classes('ui', 'form')}
+        class={classes(
+          'ui',
+          this.size,
+          'form',
+        )}
       >
         {this.$slots.default}
       </ElementType>
