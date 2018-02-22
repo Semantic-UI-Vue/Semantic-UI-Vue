0e2ac7a0ffdc34cbd0e0717e3aa8276ee4c2abf9
Arsen <arsen.stahanov@gmail.com>
2018-02-22 18:59:27 +0200

    [FormFields] Add `width` prop

--- src/collections/Form/FormField.jsx
+++ src/collections/Form/FormField.jsx
@@ -1,13 +1,23 @@
-import { classes, getChildProps, getElementType } from '../../lib';
+import { num, classes, getChildProps, getElementType } from '../../lib';
+import { Enum } from '../../lib/PropTypes';
 
 export default {
   name: 'SuiFormField',
+  props: {
+    width: {
+      type: Number,
+      description: 'A field can specify its width in grid columns',
+    },
+  },
   render() {
     const ElementType = getElementType(this);
     return (
       <ElementType
         {...getChildProps(this)}
-        class={classes('field')}
+        class={classes(
+          num(this.width),
+          'field',
+        )}
       >
         {this.$slots.default}
       </ElementType>
