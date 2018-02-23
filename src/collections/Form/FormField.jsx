b3bf37fe4dad2ceb52ee7ec3b43e2903dca0a04b
Arsen Stahanov <30985699+ArStah@users.noreply.github.com>
2018-02-23 14:37:39 +0200

    Add missing props to Form (#102)
    * Add `fields` prop to FormFields
    
    * [Form] Add `size` prop
    
    * [Form] Add loading prop
    
    * [Form] Add `success`, `warning`, `error` props
    
    * [Form] Add `inverted` prop
    
    * [Form] add `equalWidth` prop
    
    * [Form] Add `state` prop
    
    * [FormFields] Add `width` prop
    
    * [FormField] Add `inline` prop
    
    * [FormField] Add `required` prop
    
    * [FormField] Add `disabled` prop
    
    * [FormFields] Add description to props
    
    * [Form] Add `unstackable` prop
    
    * [Form*] Add usage of Enum.Number
    
    * [FormFields] Fix Enum is undefined
    
    * [FormField] Add `error` prop
    
    * [Form] Add default tag
    
    * [Test] Add tests

--- src/collections/Form/FormField.jsx
+++ src/collections/Form/FormField.jsx
@@ -1,13 +1,42 @@
-import { classes, getChildProps, getElementType } from '../../lib';
+import { num, classes, getChildProps, getElementType } from '../../lib';
+import { Enum } from '../../lib/PropTypes';
 
 export default {
   name: 'SuiFormField',
+  props: {
+    width: Enum.Number({
+      description: 'A field can specify its width in grid columns',
+    }),
+    inline: {
+      type: Boolean,
+      description: 'A field can have its label next to instead of above it.',
+    },
+    required: {
+      type: Boolean,
+      description: 'A field can show that input is mandatory.',
+    },
+    disabled: {
+      type: Boolean,
+      description: 'Individual fields may be disabled.',
+    },
+    error: {
+      type: Boolean,
+      description: 'Individual fields may display an error state.',
+    },
+  },
   render() {
     const ElementType = getElementType(this);
     return (
       <ElementType
         {...getChildProps(this)}
-        class={classes('field')}
+        class={classes(
+          num(this.width),
+          this.inline && 'inline',
+          this.required && 'required',
+          this.disabled && 'disabled',
+          'field',
+          this.error && 'error',
+        )}
       >
         {this.$slots.default}
       </ElementType>
