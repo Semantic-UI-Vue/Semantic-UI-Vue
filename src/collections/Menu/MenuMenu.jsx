ad85771244622d82b79254274c8bbfb02be3c518
Arsen Stahanov <arsen.stahanov@gmail.com>
2018-03-17 17:49:40 +0200

    [MenuMenu] Add missing descriptions

--- src/collections/Menu/MenuMenu.jsx
+++ src/collections/Menu/MenuMenu.jsx
@@ -5,7 +5,9 @@ export default {
   name: 'SuiMenuMenu',
   mixins: [listenersMixin],
   props: {
-    position: Enum(['left', 'right']),
+    position: Enum(['left', 'right'], {
+      description: 'A sub menu can take left or right position',
+    }),
   },
   render() {
     const ElementType = getElementType(this);
