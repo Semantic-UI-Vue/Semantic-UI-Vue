cf26019007a37837a53f110805691fb28c04720d
sunny yuen <yuens1002@gmail.com>
2018-03-21 17:23:20 -0400

    added secondary menu placeholder (#146)

---
+++ docs/examples/collections/MenuExample/SecondaryMenu.example.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <sui-menu secondary>
+    <sui-menu-item active link name="Home">Home</sui-menu-item>
+    <sui-menu-item name="Messages">Messages</sui-menu-item>
+    <sui-menu-item name="Friends">Friends</sui-menu-item>
+  </sui-menu>
+</template>
+
+<script>
+export default {
+  name: 'SecondaryMenuExample',
+};
+</script>
