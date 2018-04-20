6a84f5ab2da12a2e398a8decc448e097984d40e1
Arsen Stahanov <arsen.stahanov@gmail.com>
2018-04-20 06:55:09 +0300

    Add missing props to Message

--- src/collections/Message/Message.jsx
+++ src/collections/Message/Message.jsx
@@ -1,4 +1,5 @@
 import { SemanticUIVueMixin } from '../../lib';
+import { Enum } from '../../lib/PropTypes';
 import SuiIcon from '../../elements/Icon/Icon';
 import SuiMessageContent from './MessageContent';
 import SuiMessageHeader from './MessageHeader';
@@ -10,15 +11,68 @@ export default {
   components: { SuiIcon, SuiMessageContent, SuiMessageHeader, SuiMessageItem, SuiMessageList },
   mixins: [SemanticUIVueMixin],
   props: {
-    content: String,
-    dismissable: Boolean,
-    header: String,
-    icon: [Boolean, String],
+    content: {
+      type: String,
+      description: 'Shorthand for primary content.',
+    },
+    dismissable: {
+      type: Boolean,
+      description: 'A message can be dismissable',
+    },
+    header: {
+      type: String,
+      description: 'Shorthand for SuiMessageHeader.',
+    },
+    icon: {
+      type: [Boolean, String],
+      description: 'A message can contain an icon.',
+    },
+    list: {
+      type: Array,
+      description: 'Array shorthand items for the SuiMessageList',
+    },
+    size: Enum(['mini', 'tiny', 'small', 'large', 'big', 'huge', 'massive'], {
+      description: 'A message can have different sizes.',
+    }),
+    attached: Enum(['bottom', 'top'], {
+      type: Boolean,
+      description: 'A message can be formatted to attach itself to other content.',
+    }),
+    color: Enum.Color({
+      description: 'A message can be formatted to be different colors.',
+    }),
+    compact: {
+      type: Boolean,
+      description: 'A message can only take up the width of its content.',
+    },
+    floating: {
+      type: Boolean,
+      description: 'A message can float above content that it is related to.',
+    },
     info: {
       type: Boolean,
       description: 'A message may be formatted to display information.',
     },
-    list: Array,
+    warning: {
+      type: Boolean,
+      description: 'A message may be formatted to display warning messages.',
+    },
+    error: {
+      type: Boolean,
+      description: 'A message may be formatted to display a negative message. Same as `negative`.',
+    },
+    negative: {
+      type: Boolean,
+      description: 'A message may be formatted to display a negative message. Same as `error`.',
+    },
+    success: {
+      type: Boolean,
+      description: 'A message may be formatted to display a positive message. Same as `positive`.',
+    },
+    positive: {
+      type: Boolean,
+      description: 'A message may be formatted to display a positive message. Same as `success`.',
+    },
   },
   events: {
     dismiss: {
@@ -50,12 +104,23 @@ export default {
         {...this.getChildPropsAndListeners()}
         class={this.classes(
           'ui',
-          this.icon && 'icon',
+          this.color,
+          this.size,
+          this.floating && 'floating',
           this.info && 'info',
+          this.warning && 'warning',
+          this.success && 'success',
+          this.positive && 'positive',
+          this.error && 'error',
+          this.negative && 'negative',
+          this.attached, this.attached && 'attached',
+          this.icon && 'icon',
+          this.compact && 'compact',
           'message',
         )}
       >
-        {this.dismissable && <SuiIcon name="close" nativeOnClick={this.handleDismiss} />}
+        {this.dismissable && <SuiIcon name="close"
+                                      nativeOnClick={this.handleDismiss} />}
         {icon && <SuiIcon name={icon} />}
         {icon && <SuiMessageContent>{content}</SuiMessageContent>}
         {!icon && content}
