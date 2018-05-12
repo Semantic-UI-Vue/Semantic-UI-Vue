0963afa33dbb88d0587b3d644eb0cb67b2938d1a
Mario Lamacchia <mlamacchia@hubspot.com>
2018-05-12 13:16:23 +0100

    Add stretched grid and examples

---
+++ docs/examples/collections/GridExample/Columns.example.vue
@@ -0,0 +1,27 @@
+<template>
+  <sui-grid>
+    <sui-grid-row>
+      <sui-grid-column :width="8">
+        <docs-wireframe name="paragraph" />
+      </sui-grid-column>
+      <sui-grid-column :width="8">
+        <docs-wireframe name="paragraph" />
+      </sui-grid-column>
+    </sui-grid-row>
+
+    <sui-grid-row>
+      <sui-grid-column :width="8">
+        <docs-wireframe name="paragraph" />
+      </sui-grid-column>
+      <sui-grid-column :width="8">
+        <docs-wireframe name="paragraph" />
+      </sui-grid-column>
+    </sui-grid-row>
+  </sui-grid>
+</template>
+
+<script>
+export default {
+  name: 'ColumnsExample',
+};
+</script>
