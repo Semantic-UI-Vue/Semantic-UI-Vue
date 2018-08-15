9049cf4f0e65dcc745384af7762807831ef029e5
Dom Corvasce <dom.corvasce@yandex.com>
2018-08-15 18:29:56 +0200

    Add inverted prop to list (#208)
    * Add inverted prop to list
    
    * Add spec for list
    
    * Add example for inverted list

---
+++ docs/examples/elements/ListExample/ListInverted.example.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <sui-segment inverted>
+    <sui-list inverted divided relaxed>
+      <sui-list-item>Apples</sui-list-item>
+      <sui-list-item>Pears</sui-list-item>
+      <sui-list-item>Oranges</sui-list-item>
+    </sui-list>
+  </sui-segment>
+</template>
+
+<script>
+export default {
+  name: 'ListInvertedExample',
+};
+</script>
--- docs/examples/elements/ListExample/index.js
+++ docs/examples/elements/ListExample/index.js
@@ -3,6 +3,7 @@ import ListShorthand from './ListShorthand.example';
 import ListIcon from './ListIcon.example';
 import ListDivided from './ListDivided.example';
 import ListTree from './ListTree.example';
+import ListInverted from './ListInverted.example';
 
 export default [
   {
@@ -12,6 +13,11 @@ export default [
         title: 'List',
         description: 'A list groups related content',
         component: List,
+      },
+      {
+        title: 'Inverted',
+        description: 'A list can be inverted to appear on a dark background',
+        component: ListInverted,
       },
       {
         description: 'You can also pass an array of items as props',
