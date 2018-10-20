62317f54252dec861d36bba9e585e485d2f98eb5
Mario Lamacchia <mlamacchia@hubspot.com>
2018-10-20 13:56:08 +0100

    Remove old code

--- docs/examples/modules/PopupExample/index.js
+++ docs/examples/modules/PopupExample/index.js
@@ -1,56 +1,56 @@
-import Popup from './Popup.example';
-import Titled from './Titled.example';
-import Html from './Html.example';
-import Basic from './Basic.example';
-import Width from './Width.example';
-import Fluid from './Fluid.example';
-import Size from './Size.example';
-import Flowing from './Flowing.example';
-import Inverted from './Inverted.example';
+// import Popup from './Popup.example';
+// import Titled from './Titled.example';
+// import Html from './Html.example';
+// import Basic from './Basic.example';
+// import Width from './Width.example';
+// import Fluid from './Fluid.example';
+// import Size from './Size.example';
+// import Flowing from './Flowing.example';
+// import Inverted from './Inverted.example';
 import Position from './Position.example';
 
 export default [{
   title: 'Types',
   examples: [{
-    title: 'Popup',
-    description: 'An element can specify popup content to appear.',
-    component: Popup,
-  }, {
-    title: 'Titled',
-    description: 'An element can specify popup content with a title.',
-    component: Titled,
-  }, {
-    title: 'Html',
-    description: 'An element can specify HTML for a popup.',
-    component: Html,
-  }],
-}, {
-  title: 'Variations',
-  examples: [{
-    title: 'Basic',
-    description: 'An element can specify a simple tooltip that can appear without javascript',
-    component: Basic,
-  }, {
-    title: 'Width',
-    description: 'A popup can be extra wide to allow for longer content.',
-    component: Width,
-  }, {
-    title: 'Fluid',
-    description: 'A fluid popup will take up the entire width of its offset container.',
-    component: Fluid,
-  }, {
-    title: 'Size',
-    description: 'A popup can vary in size.',
-    component: Size,
-  }, {
-    title: 'Flowing',
-    description: 'A popup can have no maximum width and continue to flow to fit its content.',
-    component: Flowing,
-  }, {
-    title: 'Inverted',
-    description: 'A popup can have its colors inverted.',
-    component: Inverted,
-  }, {
+//     title: 'Popup',
+//     description: 'An element can specify popup content to appear.',
+//     component: Popup,
+//   }, {
+//     title: 'Titled',
+//     description: 'An element can specify popup content with a title.',
+//     component: Titled,
+//   }, {
+//     title: 'Html',
+//     description: 'An element can specify HTML for a popup.',
+//     component: Html,
+//   }],
+// }, {
+//   title: 'Variations',
+//   examples: [{
+//     title: 'Basic',
+//     description: 'An element can specify a simple tooltip that can appear without javascript',
+//     component: Basic,
+//   }, {
+//     title: 'Width',
+//     description: 'A popup can be extra wide to allow for longer content.',
+//     component: Width,
+//   }, {
+//     title: 'Fluid',
+//     description: 'A fluid popup will take up the entire width of its offset container.',
+//     component: Fluid,
+//   }, {
+//     title: 'Size',
+//     description: 'A popup can vary in size.',
+//     component: Size,
+//   }, {
+//     title: 'Flowing',
+//     description: 'A popup can have no maximum width and continue to flow to fit its content.',
+//     component: Flowing,
+//   }, {
+//     title: 'Inverted',
+//     description: 'A popup can have its colors inverted.',
+//     component: Inverted,
+//   }, {
     title: 'Position',
     description: 'A popup can be position around its trigger.',
     component: Position,
--- src/modules/Popup/private/PopupContainer.jsx
+++ src/modules/Popup/private/PopupContainer.jsx
@@ -1,7 +1,5 @@
 import mapValues from 'lodash/mapValues';
-import without from 'lodash/without';
 import { isBrowser, SemanticUIVueMixin } from '../../../lib';
-import { POSITIONS } from './popupConstants';
 
 export default {
   name: 'SuiPrivatePopupContainer',
@@ -30,16 +28,18 @@ export default {
       if (!isBrowser) return style;
 
       const { offset, triggerElm } = this;
-      const { pageYOffset, pageXOffset } = window;
-      const { clientWidth, clientHeight } = document.documentElement;
       const { offsetHeight, offsetWidth } = this.$el;
+
+      style.width = offsetWidth;
 
       if (positions.includes('right')) {
         style.left = Math.round(triggerElm.offsetLeft + triggerElm.offsetWidth - offsetWidth);
       } else if (positions.includes('left')) {
         style.left = Math.round(triggerElm.offsetLeft);
       } else { // if not left nor right, we are horizontally centering the element
-        style.left = Math.round(triggerElm.offsetLeft + (triggerElm.offsetWidth / 2) - (offsetWidth / 2));
+        style.left = Math.round(
+          triggerElm.offsetLeft + (triggerElm.offsetWidth / 2) - (offsetWidth / 2),
+        );
       }
 
       if (positions.includes('top')) {
@@ -47,7 +47,9 @@ export default {
       } else if (positions.includes('bottom')) {
         style.top = Math.round(this.triggerElm.offsetTop + this.triggerElm.offsetHeight);
       } else { // if not top nor bottom, we are vertically centering the element
-        style.top = Math.round(this.triggerElm.offsetTop + (triggerElm.offsetHeight / 2) - (offsetHeight / 2));
+        style.top = Math.round(
+          this.triggerElm.offsetTop + (triggerElm.offsetHeight / 2) - (offsetHeight / 2),
+        );
 
         const xOffset = offsetWidth + 8;
         if (positions.includes('right')) {
@@ -67,57 +69,16 @@ export default {
 
       return style;
     },
-    isStyleInViewport(style) {
-      return true;
-      const { pageYOffset, pageXOffset } = window;
-      const { clientWidth, clientHeight } = document.documentElement;
-
-      const element = {
-        top: style.top,
-        left: style.left,
-        width: this.popupCoords.width,
-        height: this.popupCoords.height,
-      };
-
-      if (typeof style.right === 'number') {
-        element.left = clientWidth - style.right - element.width;
-      }
-      if (typeof style.bottom === 'number') {
-        element.top = clientHeight - style.bottom - element.height;
-      }
-
-      // hidden on top
-      if (element.top < pageYOffset) return false;
-      // hidden on the bottom
-      if (element.top + element.height > pageYOffset + clientHeight) return false;
-      // hidden the left
-      if (element.left < pageXOffset) return false;
-      // hidden on the right
-      if (element.left + element.width > pageXOffset + clientWidth) return false;
-
-      return true;
-    },
     setPopupStyle() {
       if (!this.triggerCoords || !this.popupCoords) return;
-      let { position } = this;
-      let style = this.computePopupStyle(position);
+      const { position } = this;
 
-      // Lets detect if the popup is out of the viewport and adjust
-      // the position accordingly
-      const positions = without(POSITIONS, position).concat([position]);
-      for (let i = 0; !this.isStyleInViewport(style) && i < positions.length; i += 1) {
-        style = this.computePopupStyle(positions[i]);
-        position = positions[i];
-      }
+      let style = this.computePopupStyle(position);
 
       // Append 'px' to every numerical values in the style
       style = mapValues(style, value => (typeof value === 'number' ? `${value}px` : value));
       this.mountedStyle = style;
       this.mountedPosition = position;
-    },
-    handlePopupMounted() {
-      this.popupCoords = this.$el.getBoundingClientRect();
-      this.setPopupStyle();
     },
   },
   render() {
