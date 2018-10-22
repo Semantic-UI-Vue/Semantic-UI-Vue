1acabe01bc0a61b4d519ac78522c3aebb67a64a9
Maxim Ilin <ilin@softservice.org>
2018-10-22 11:17:58 +0700

    Added search in menu property.

--- src/modules/Dropdown/Dropdown.jsx
+++ src/modules/Dropdown/Dropdown.jsx
@@ -2,6 +2,8 @@ import escapeRegExp from 'lodash/escapeRegExp';
 import { SemanticUIVueMixin } from '../../lib';
 import { Enum } from '../../lib/PropTypes';
 import Icon from '../../elements/Icon/Icon';
+import Input from '../../elements/Input/Input';
+import Divider from '../../elements/Divider/Divider';
 import Label from '../../elements/Label/Label';
 import DropdownItem from './DropdownItem';
 import DropdownMenu from './DropdownMenu';
@@ -86,6 +88,10 @@ export default {
       type: Boolean,
       description: 'A dropdown can have a search field to filter options.',
     },
+    searchInMenu: {
+      type: Object,
+      description: "A dropdown can have a search input in dropdown menu. Should be passed an Object with SuiInput props."
+    },
     selection: {
       type: Boolean,
       description: 'A dropdown can be used to select between choices in a form.',
@@ -159,7 +165,7 @@ export default {
       return `${animations.name} ${(this.downward ? animations.down : animations.up)}`;
     },
     filteredOptions() {
-      if (!this.search && !this.multiple) {
+      if (!this.search && !this.multiple && !this.searchInMenu) {
         return this.options;
       }
       const re = new RegExp(escapeRegExp(this.filter), 'i');
@@ -197,7 +203,12 @@ export default {
     menuNode() {
       return (
         <DropdownMenu>
-          {
+          {[
+            this.searchInMenu && [<Input {...{props: this.searchInMenu, ref: 'searchInMenu'}}
+                                         onInput={this.updateFilter}
+                                         value={this.filter}
+                                         onKeydown={this.handleSearchKeyDown}
+            />,<Divider/>],
             this.message ? <div class="message">{this.message}</div> : this.filteredOptions.map((option, index) => (
               <DropdownItem
                 {...{ props: option }}
@@ -208,7 +219,7 @@ export default {
                 onSelect={this.selectItem}
               />
             ))
-          }
+          ]}
         </DropdownMenu>
       );
     },
@@ -269,7 +280,7 @@ export default {
 
       const className = this.classes(
         this.placeholder && !shouldHideText && 'default',
-        this.filter && !shouldShowSelectedItem && 'filtered',
+        !this.searchInMenu && this.filter && !shouldShowSelectedItem && 'filtered',
         'text',
       );
 
@@ -288,7 +299,7 @@ export default {
       this.updateSelectedIndex();
     },
     filter() {
-      this.resizeInput();
+      this.search && this.resizeInput();
     },
   },
   mounted() {
@@ -327,6 +338,7 @@ export default {
       e.stopPropagation();
       if (this.open) {
         if (this.search && e.target === this.$refs.search) return;
+        if (this.searchInMenu && e.target === this.$refs.searchInMenu.$refs.input) return;
         if (this.multiple && e.path.indexOf(this.menu.$el) !== -1) {
           this.$nextTick(() => this.focusSearch());
           return;
@@ -355,7 +367,7 @@ export default {
       e.stopPropagation();
     },
     toggleFilteredText(filteredText, filter) {
-      if (!this.multiple && !filteredText.classList.contains('filtered') && filter.trim() !== '') {
+      if (!this.searchInMenu && !this.multiple && !filteredText.classList.contains('filtered') && filter.trim() !== '') {
         filteredText.classList.add('filtered');
       }
 
@@ -387,7 +399,7 @@ export default {
           if (this.allowAdditions && this.selectedIndex === -1 && filter.trim() !== '') {
             e.preventDefault();
             this.selectItem(filter);
-          } else if (this.selection) {
+          } else if (this.selection || this.searchInMenu || this.search) {
             if (this.selectedIndex === -1) return;
             e.preventDefault();
             if (!this.multiple) {
@@ -420,7 +432,7 @@ export default {
       } else {
         this.selectedIndex = newValue;
       }
-      if (this.selection && !this.multiple) {
+      if ((this.selection || this.searchInMenu || this.search) && !this.multiple) {
         this.$emit('input', this.filteredOptions[this.selectedIndex].value);
       }
     },
@@ -456,7 +468,7 @@ export default {
       this.$refs.search.style.minWidth = `${Math.ceil(width + 1)}px`;
     },
     updateFilter(event) {
-      this.filter = event.target.value;
+      this.filter = typeof event === "string" ? event : event.target.value;
     },
     focusSearch() {
       if (this.search) this.$refs.search.focus();
