cd38136dd8c140d37413b771e115d615868d3c7c
jakubsaleniuk <jakub.saleniuk@gmail.com>
2019-03-04 23:19:09 +0100

    First version of search component

---
+++ src/modules/Search/Search.jsx
@@ -0,0 +1,73 @@
+import { SemanticUIVueMixin } from '../../lib';
+
+export default {
+  name: 'SuiRating',
+  mixins: [SemanticUIVueMixin],
+  binding: {
+    prop: 'rating',
+    event: 'changed',
+  },
+  props: {
+    icon: String,
+    maxRating: Number,
+    rating: Number,
+  },
+  events: {
+    rate: {
+      custom: true,
+    },
+  },
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    getCurrentValue(evt) {
+      return Number(evt.target.getAttribute('aria-posinset'));
+    },
+    onRate(evt) {
+      const rating = this.getCurrentValue(evt);
+      this.$emit('rate', evt, { ...this.$props, rating });
+    },
+    onMouseleave() {
+      this.selected = 0;
+    },
+    onMouseover(evt) {
+      this.selected = this.getCurrentValue(evt);
+    },
+  },
+  render() {
+    const ElementType = this.getElementType();
+    return (
+      <ElementType
+        {...this.getChildPropsAndListeners()}
+        class={this.classes(
+          'ui',
+          this.icon,
+          'rating',
+        )}
+        role="radiogroup"
+      >
+        {[...new Array(this.maxRating)].map((v, i) => {
+          const elementValue = i + 1;
+          const active = this.rating > i;
+          const selected = this.selected > i;
+          return (
+            <i
+              aria-checked={active.toString()}
+              aria-posinset={elementValue}
+              aria-setsize={this.maxRating}
+              class={this.classes(active && 'active', selected && 'selected', 'icon')}
+              tabindex="0"
+              role="radio"
+              onClick={this.onRate}
+              onMouseover={this.onMouseover}
+              onMouseleave={this.onMouseleave}
+            />
+          );
+        })}
+      </ElementType>
+    );
+  },
+};
