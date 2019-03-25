904f27e3d2386310e0cbb3e7c0b55b5e8d68f707
raynox <jakub.saleniuk@gmail.com>
2019-03-25 17:37:30 +0100

    Search component (#309)
    * Search component - minCharacters
    
    * Search component - minCharacters
    
    * Search component - input icon

--- src/modules/Search/Search.jsx
+++ src/modules/Search/Search.jsx
@@ -21,6 +21,11 @@ export default {
       default: 1,
       descriptiom: 'Minimum characters to query for results',
     },
+    input: {
+      type: Boolean,
+      default: false,
+      description: 'Whether the search component should has input class.',
+    },
   },
   data() {
     return {
@@ -58,6 +63,17 @@ export default {
         this.firstSearch = true;
       }
     },
+    renderInput() {
+      let input = <input onBlur={this.handleBlur}
+                    onFocus={this.handleFocus}
+                    onInput={this.handleInput}
+                    value={this.value}
+                    class={this.classes('prompt')}
+                    {...{ attrs: this.$attrs }}/>;
+      input = this.input ? <div class={this.classes('ui', 'icon', 'input')}>{input}{this.$slots.icon}</div> : input;
+
+      return input;
+    },
   },
   render() {
     const ElementType = this.getElementType();
@@ -68,12 +84,7 @@ export default {
                 'ui',
                 'search',
             )}>
-          <input onBlur={this.handleBlur}
-                 onFocus={this.handleFocus}
-                 onInput={this.handleInput}
-                 value={this.value}
-                 class={this.classes('prompt')}
-                 {...{ attrs: this.$attrs }}/>
+          {this.renderInput()}
           {this.firstSearch &&
           <Results query={this.value} source={this.source} visible={this.resultsVisible}/>
           }
