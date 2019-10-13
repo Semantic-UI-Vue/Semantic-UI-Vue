ace37c69a940b653de74550043908f9bae0e18e2
franciscoemanuel <francisco.emanuel@hotmail.com.br>
2019-10-13 18:03:19 -0300

    Adds Message state hidden and visible examples

---
+++ docs/examples/collections/MessageExample/Hidden.example.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div>
+    <sui-message hidden>
+      <p>You can't see me</p>
+    </sui-message>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HiddenMessageExample',
+};
+</script>
