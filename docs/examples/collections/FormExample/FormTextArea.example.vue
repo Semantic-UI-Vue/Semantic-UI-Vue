2a5d93b213992b7ce753d34c07a51fd5d0e010cd
Maxim Makarov <32133426+longsightedfilms@users.noreply.github.com>
2019-11-12 11:46:42 +0300

    [Form] Completing form collection (#418)
    * Fixed mismatched prop type for container component
    
    * Completing Icon element by features, updated documentation
    
    * Implemented Icon Group element
    
    * Added missing enum corner, updated docs
    
    * Completed documentation for form collection

---
+++ docs/examples/collections/FormExample/FormTextArea.example.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <sui-form>
+    <sui-form-field>
+      <label>Text</label>
+      <textarea></textarea>
+    </sui-form-field>
+    <sui-form-field>
+      <label>Short Text</label>
+      <textarea rows="2"></textarea>
+    </sui-form-field>
+  </sui-form>
+</template>
+
+<script>
+export default {
+  name: 'FormTextAreaExample',
+};
+</script>
